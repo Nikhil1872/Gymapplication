ac2a78f73a465be72037192f3522052f77b5a8d2
Nikhil Talmale <114892182+Nikhil1872@users.noreply.github.com>
2023-06-21 23:42:29 +0530

    Update Gym-sql.sql

--- Gym-sql.sql
+++ Gym-sql.sql
@@ -2,4 +2,15 @@
 
 create database gymdata1;
 use gymdata1;
+CREATE TABLE user (
+  email VARCHAR(20),
+  adharno INTEGER(12) UNIQUE,
+  age INTEGER(2),
+  gender VARCHAR(7),
+  mno INTEGER(10),
+  password VARCHAR(15) UNIQUE,
+  uname VARCHAR(10) PRIMARY KEY
+);
+
+
 select * from user;
